e56078ad6c4bd5a8959a372fe760a3fe77af31bb
claytongentry <claytongentry2017@u.northwestern.edu>
2018-10-02 16:29:44 -0400

    support cookie auth

--- test/plug_auth_test.exs
+++ test/plug_auth_test.exs
@@ -1,7 +1,35 @@
 defmodule InverseAuthTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @param "my-token"
+  @token "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMTIzLXRlc3QifQ.jPUatqZfWkdKTHSvcGSjAmERAXmqj8wUM1OVfSBQAznQxsZxLxZ9nk60TgEI8BDNvIDIlG0i4Dqe-E5a8AJBbg"
+
+  test "401s requests without authorization parameters" do
+    opts = InverseAuth.Plug.init([auth: TestAuth, param: @param])
+    conn =
+      :post
+      |> conn("/")
+      |> InverseAuth.Plug.call(opts)
+
+    assert conn.status == 401
+    assert conn.halted
   end
+
+  test "supports cookie-based auth" do
+    Application.put_env(:inverse_auth, :jwt_secret, "fssecret")
+    opts = InverseAuth.Plug.init([auth: TestAuth, param: @param])
+    conn =
+      :post
+      |> conn("/")
+      |> put_req_cookie(@param, @token)
+      |> InverseAuth.Plug.call(opts)
+
+    assert is_nil(conn.status)
+    refute conn.halted
+  end
+end
+
+defmodule TestAuth do
+  def authenticate(conn), do: {:ok, conn}
 end
